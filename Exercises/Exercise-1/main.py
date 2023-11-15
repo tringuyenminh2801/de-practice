f3469f2c6469783dc83a434fc041acea3563c159
tringuyenminh2801 <trinm2801@gmail.com>
2023-11-16 00:10:05 +0700

    Update Ex1 and Ex2

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,3 +1,6 @@
+import os
+import time
+import zipfile
 import requests
 
 download_uris = [
@@ -10,9 +13,56 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+downloadDirName = "downloads"
 
+def timeIt(function):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = function(*args, **kwargs)
+        end = time.time()
+        print(f"{function.__name__} ran in {end - start:.3f} seconds\n")
+        return res
+    return wrapper
+
+@timeIt
+def createDirectory(dirName: str):
+    workingDir = os.curdir
+    newDirAbsolutePath = os.path.join(workingDir, dirName)
+    if not os.path.isdir(newDirAbsolutePath):
+        print(f"{dirName} not exist, creating a new one...")
+        os.mkdir(newDirAbsolutePath)
+
+@timeIt
+def downloadFileByURL(url: str) -> bytes:
+    r = requests.get(url=url, allow_redirects=True)
+    return r.content
+
+@timeIt
+def unzipFile(currentDir: str, zipfileName: str):
+    with zipfile.ZipFile(os.path.join(currentDir, zipfileName), 'r') as zip_ref:
+        zip_ref.extractall(path=currentDir)
+
+@timeIt
 def main():
-    # your code here
+    createDirectory(dirName=downloadDirName)
+    for uri in download_uris:
+        downloadFolderName = os.path.join(os.curdir, downloadDirName)
+        zipfileName = uri.split("/")[-1]
+        print(f"Download zip file of {zipfileName}...")
+        try:
+            # DOWNLOAD THE FILE
+            content = downloadFileByURL(url=uri)
+            with open(os.path.join(downloadFolderName, zipfileName), "wb") as f:
+                f.write(content)
+            # UNZIP THE FILE
+            print(f"Unzipp and extract {zipfileName}...")
+            unzipFile(currentDir=downloadFolderName, zipfileName=zipfileName)
+            # REMOVE THE ZIP FILE
+            print(f"Removing {zipfileName}...")
+            os.remove(os.path.join(downloadFolderName, zipfileName))
+            print(f"Done {zipfileName}!\n--------------------------")
+        except Exception as e:
+            print(f"Exception occur for {zipfileName}: {e}\n--------------------------")
     pass
 
 
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,46 @@
 import requests
-import pandas
+import polars as pl
 
+from bs4 import BeautifulSoup
+
+web_url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
+
+def getPageContent(url: str) -> str:
+    r = requests.get(url=url, allow_redirects=True)
+    print(f"Status code: {r.status_code}")
+    soup = BeautifulSoup(markup=r.content, features='html.parser')
+    if r.status_code == 200:
+        page_content = soup.text
+    else:
+        print(f"Cannot get the content of {url}\n")
+        page_content = None
+    return page_content
+
+def findFileNameByPattern(pageContentByLines: list[str], pattern: str, extension: str) -> str:
+    for line in pageContentByLines:
+        if pattern in line:
+            fileName = f"{line.split(sep=extension)[0]}{extension}"
+            break
+    return fileName
 
 def main():
-    # your code here
-    pass
-
+    pageContent = getPageContent(url=web_url)
+    if pageContent is None:
+        print(f"Found nothing from the web, exit the program")
+        exit()
+    pageContentByLines = pageContent.split(sep="\n")
+    print("Find file name...")
+    fileName = findFileNameByPattern(pageContentByLines=pageContentByLines,
+                                     pattern="2022-02-07 14:03",
+                                     extension=".csv")
+    print(f"Found file name: {fileName}")
+    fileLink = f"{web_url}{fileName}"
+    print(f"Download the file from url {fileLink}...")
+    r = requests.get(url=fileLink, allow_redirects=True)
+    df = pl.read_csv(source=r.content) if r.status_code == 200 else None
+    if df is not None:
+        df = df.filter(pl.col("HourlyDryBulbTemperature") == pl.col("HourlyDryBulbTemperature").max())
+        print(df)
 
 if __name__ == "__main__":
     main()
